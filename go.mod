57ec9f3e65f8f79aa6b01d907c6c6962a6041262
pirespsps <matheusknoll28@gmail.com>
2025-11-07 23:28:13 -0300

    Feito

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pirespsps/path-dependency-adder
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+
+	const filepath = "/home/pires/.bashrc"
+
+	file, err := os.OpenFile(filepath, os.O_APPEND|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	command := "export PATH=$PATH: " + path
+
+	file.WriteString(command)
+
+	file.Sync()
+
+	cmd := exec.Command(command)
+	defer cmd.Run()
+
+	fmt.Print("ok\n")
+
+}
